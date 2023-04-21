2fd00e74add93163cb91dc0ef000b3d2f4cd8585
Szaniszló Dávid <h164341@stud.u-szeged.hu>
2023-04-21 11:40:23 +0200

    add room design

---
+++ app/src/main/java/com/example/szallashelyfoglalas/RoomAdd.java
@@ -0,0 +1,14 @@
+package com.example.szallashelyfoglalas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RoomAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_add);
+    }
+}
